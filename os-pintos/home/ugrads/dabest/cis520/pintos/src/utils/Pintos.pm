28847eb30a0342164b10b2c1d42e76ec0d2f1c13
Wendy <wchong@hawaii.edu>
2017-09-28 16:59:19 -1000

    corrected file paths and able to run test on loaded pintos

--- os-pintos/home/ugrads/dabest/cis520/pintos/src/utils/Pintos.pm
+++ os-pintos/home/ugrads/dabest/cis520/pintos/src/utils/Pintos.pm
@@ -359,7 +359,7 @@ sub cyl_sectors {
 # Makes sure that the loader is a reasonable size.
 sub read_loader {
     my ($name) = @_;
-    $name = find_file ("/home/wendy/os-pintos/home/ugrads/dabest/cis520/pintos/src/threads/build/loader.bin") if !defined $name;
+    $name = find_file ("/home/wendy/pintos/os-pintos/home/ugrads/dabest/cis520/pintos/src/threads/build/loader.bin") if !defined $name;
     die "Cannot find loader\n" if !defined $name;
 
     my ($handle);
